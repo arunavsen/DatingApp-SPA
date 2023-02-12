64b51eace84a30d513e26ba289c27e2f0a784e95
arunav sen <arunavsen96@gmail.com>
2023-02-13 00:49:37 +0600

    Parent to child component communication using input properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import {HttpClientModule} from '@angular/common/http'
 import { AnimalComponent } from './animal/animal.component';
 import { NavComponent } from './Nav/Nav.component';
@@ -14,7 +13,6 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [						
     AppComponent,
-      ValueComponent,
       AnimalComponent,
       NavComponent,
       HomeComponent,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,16 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-
-  constructor() { }
+  values : any;
+  
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues();
   }
-
 
   registerToggle(){
     debugger
     this.registerMode = !this.registerMode;
+  }
+
+  getValues(){
+    this.http.get('http://localhost:5010/values').subscribe(result => {
+      this.values = result;
+    }, error => {
+        console.log(error);
+    })
   }
 
 }
