86049f6564d22926ad7dd583354d16716889305c
arunav sen <arunavsen96@gmail.com>
2023-04-29 13:13:08 +0600

    Use a route guard

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,10 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { MessegesComponent } from './messeges/messeges.component';
 import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'members', component: MemberListComponent },
+    { path: 'members', component: MemberListComponent, canActivate:[AuthGuard] },
     { path: 'messeges', component: MessegesComponent },
     { path: 'lists', component: ListsComponent },
     { path: '**', redirectTo: 'home', pathMatch: 'full' } // If no routes match, then '**' it will automatically redirect it to 'home'
