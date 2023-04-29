be9990df8a10686f71f470673d24ec775ff02f8e
arunav sen <arunavsen96@gmail.com>
2023-04-29 13:43:27 +0600

    Protecting multiple routes with single route guard

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,9 +6,16 @@ import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'members', component: MemberListComponent, canActivate:[AuthGuard] },
-    { path: 'messeges', component: MessegesComponent },
-    { path: 'lists', component: ListsComponent },
-    { path: '**', redirectTo: 'home', pathMatch: 'full' } // If no routes match, then '**' it will automatically redirect it to 'home'
+    { path: '', component: HomeComponent },
+    {
+      path: '',
+      runGuardsAndResolvers: 'always',
+      canActivate: [AuthGuard],
+      children: [
+        { path: 'members', component: MemberListComponent },
+        { path: 'messeges', component: MessegesComponent },
+        { path: 'lists', component: ListsComponent },
+      ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' } // If no routes match, then '**' it will automatically redirect it to 'home'
   ];
